9f197a155cd3c32358defadbac73ed14275e091b
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-22 18:47:07 +0000

    dfsbvgsdfgsg

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+const randomnumber = Math.floor(Math.random()*6)+1
+
+function checking() {
+  const usernumber = parseInt(document.getElementById("number").value)
+  if (usernumber == randomnumber) {
+    document.getElementById("result").innerHTML = "you have guessed the correct number"
+  }
+  if (usernumber!= randomnumber) {
+    document.getElementById("result").innerHTML = "you have guessed the wrong number"
+  }
+  document.getElementById("rightAnswer").innerHTML = "It is number " + randomnumber
+}
